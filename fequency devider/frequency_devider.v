179c685ac482442b0bb960c3a09514f7776e620a
apurba1911 <128996238+apurba1911@users.noreply.github.com>
2023-04-11 03:53:08 +0530

    Add files via upload

---
+++ fequency devider/frequency_devider.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+	 
+	module tff (input clk,T,output reg q=0);
+
+always @ (posedge clk)
+
+begin
+if (T)
+begin
+q<=~q;
+end
+else
+q<=q;
+
+end
+endmodule
+
+module frequency_devider(
+    input clk,Ta,Tb,
+    output Qa,Qb
+    );
+	  
+	 tff ta (.clk(clk),.T(Ta),.q(Qa));
+	 tff tb (.clk(Qa),.T(Tb),.q(Qb));
+
+
+endmodule
+
